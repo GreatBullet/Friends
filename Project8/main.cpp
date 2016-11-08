914983e0f30f87af6e40d73a87650fa258f2c21d
GreatBullet <rick.armbruster@hotmail.de>
2016-11-08 17:55:11 +0100

    Add project files.

---
+++ Project8/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class StankFist {
+public:
+	StankFist() { stinkyVar = 0; }
+private:
+	int stinkyVar;
+
+	friend void stinkyFriend(StankFist &sfo);
+};
+void stinkyFriend(StankFist &sfo) {
+	sfo.stinkyVar = 99;
+	cout << sfo.stinkyVar << endl;
+}
+	
+int main() {
+	StankFist bob;
+	stinkyFriend(bob);
+	cin.get();
+	return 0;
+}
